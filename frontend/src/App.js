2a0d7116b4ff8c3cf570995bd888c87e381da593
tracynguyen95 <nhunghng95@gmail.com>
2019-04-09 10:27:26 -0700

    Making the list items clickable

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import './css/searchbar.css';
 import 'font-awesome/css/font-awesome.min.css';
 
-function ListItem(props) {
-	return <li className="list-group-item">{props.value}</li>;
+class ListItem extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleClick = this.handleClick.bind(this);
+	}
+	handleClick(e) {
+		alert('You just choose '.concat(this.props.value));
+	}
+	render() {
+		if (this.props.enableClick === true) {
+			return <li onClick={this.handleClick} className="list-group-item">{this.props.value}</li>;
+		}
+		else {
+			return <li className="list-group-item">{this.props.value}</li>;
+		}
+	}
 }
 
 function ListItems(props) {
 	const items = props.items;
+	const enableClick = props.enableClick;
 	const listItems = items.map((item) =>
 		<ListItem key={item.id}
-					value={item.name} />					
+					value={item.name} enableClick={enableClick}/>					
 	);
 	return (
 		<ul className="list-group">
@@ -23,29 +37,32 @@ function ListItems(props) {
 
 function MondoList(props) {
 	const mondoList = props.mondoList;
+	const enableClick = props.enableClick;
 	return (
 		<div className="container">
 			<h5> Disease Index </h5>
-			<ListItems items={mondoList} />
+			<ListItems items={mondoList} enableClick={enableClick}/>
 		</div>
 	);
 }
 
 function GeneList(props) {
 	const geneList = props.geneList;
+	const enableClick = props.enableClick;
 	return (
 		<div className="container">
 			<h5> Gene List </h5>
-			<ListItems items={geneList} />
+			<ListItems items={geneList} enableClick={enableClick}/>
 		</div>
 	);
 }
 function BioModelList(props) {
 	const biomodelList = props.biomodelList;
+	const enableClick = props.enableClick;
 	return (
 		<div className="container">
 			<h5> Biomodel List </h5>
-			<ListItems items={biomodelList} />
+			<ListItems items={biomodelList} enableClick={enableClick}/>
 		</div>
 	);
 }
@@ -56,31 +73,26 @@ class SearchBar extends React.Component {
     this.state = {
 		searchText : '',
 		mondoList: [
-			{id: 1, name: 'cancer'},
-			{id: 2, name: 'diabetes'}
+			{id: 1, name: 'No Result'}
 		],
 		geneList: [
-			{id: 1, name: 'geneA'},
-			{id: 2, name: 'geneB'},
-			{id: 3, name: 'geneC'}
+			{id: 1, name: 'No Result'}
 		],
 
 		biomodelList: [
-			{id: 1, name: 'biomodelA'},
-			{id: 2, name: 'biomodelB'},
-			{id: 3, name: 'biomodelC'},
-			{id: 4, name: 'biomodelD'}
-		]
+			{id: 1, name: 'No Result'}
+		],
+		enableClick: false
 	}
     this.handleClick = this.handleClick.bind(this);
     this.handleTextChange = this.handleTextChange.bind(this);
   }
 
   handleClick(e) {
-    fetch('http://127.0.0.1:5000/api/disease/'.concat(this.state.searchText).concat('?size=5'))
+		fetch('http://127.0.0.1:5000/api/disease/'.concat(this.state.searchText).concat('?size=5'))
 			.then(response => response.json())
-			.then(data => this.setState({ mondoList: data }));	
-  }
+			.then(data => this.setState({ mondoList: data, enableClick: true }));
+	}
 
   handleTextChange(e) {
     this.setState({searchText : e.target.value})
@@ -103,13 +115,13 @@ class SearchBar extends React.Component {
         </button>
 		<div className="row">
 			<div className="col-sm-4">
-				<MondoList mondoList={this.state.mondoList} />
+				<MondoList mondoList={this.state.mondoList} enableClick={this.state.enableClick} />
 			</div>
 			<div className="col-sm-4">
-				<GeneList geneList={this.state.geneList} />
+				<GeneList geneList={this.state.geneList} enableClick={this.state.enableClick} />
 			</div>
 			<div className="col-sm-4">
-				<BioModelList biomodelList={this.state.biomodelList} />
+				<BioModelList biomodelList={this.state.biomodelList} enableClick={this.state.enableClick} />
 			</div>
 		</div>
       </div>
