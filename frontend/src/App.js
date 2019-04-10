f4a2d1acc1639d5f9b7f391fe5c530ffa57e75c9
Lance Hannestad <lhannest@sfu.ca>
2019-04-10 12:58:59 -0700

    Fixed page reload bug and setup pathwayClick

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,14 +7,15 @@ class ListItem extends React.Component {
 		super(props);
 		this.handleClick = this.handleClick.bind(this);
 	}
+
 	handleClick() {
 		this.props.onClick(this.props.index);
 	}
-		
+
 
 	render() {
 		if (this.props.isClickEnabled === true) {
-		return <a href="#" 
+		return <a href="#"
 					className="list-group-item list-group-item-action"
 					onClick={this.handleClick}>
 						{this.props.value}
@@ -32,12 +33,12 @@ function MondoList(props) {
 	const mondoList = props.mondoList;
 	const isClickEnabled = props.isClickEnabled;
 	const listItems = mondoList.map((item) =>
-		<ListItem 
+		<ListItem
 			key={item.id}
 			index={item.id}
-			value={item.name} 
+			value={item.name}
 			isClickEnabled={isClickEnabled}
-			onClick={props.onClick}/>					
+			onClick={props.onClick}/>
 	);
 	return (
 		<div className="container">
@@ -51,12 +52,12 @@ function GeneList(props) {
 	const geneList = props.geneList;
 	const isClickEnabled = props.isClickEnabled;
 	const listItems = geneList.map((item) =>
-		<ListItem 
+		<ListItem
             key={item.gene_symbol}
             index={item.gene_id}
 			value={item.gene_symbol}
             onClick={props.onClick}
-            isClickEnabled={isClickEnabled}/>					
+            isClickEnabled={isClickEnabled}/>
 	);
 	return (
 		<div className="container">
@@ -69,8 +70,12 @@ function BioModelList(props) {
 	const biomodelList = props.biomodelList;
 	const isClickEnabled = props.isClickEnabled;
 	const listItems = biomodelList.map((item) =>
-		<ListItem key={item.id}
-					value={item.name} isClickEnabled={isClickEnabled}/>					
+		<ListItem key={item.pathway_id}
+			index={item.pathway_id}
+			value={item.name}
+			onClick={props.onClick}
+			isClickEnabled={isClickEnabled}
+		/>
 	);
 	return (
 		<div className="container">
@@ -81,10 +86,6 @@ function BioModelList(props) {
 }
 
 class SearchBar extends React.Component {
-  constructor(props) {
-    super(props);    
-  }
-
   render() {
     return (
 		<form className="form-inline">
@@ -97,7 +98,7 @@ class SearchBar extends React.Component {
 				id="search"
 			/>
 			<button
-				type="submit"
+				type="button"
 				onClick={this.props.handleSearch}
 				className="btn btn-outline-success my-2 my-sm-0">
 				Search
@@ -126,53 +127,65 @@ class App extends Component {
 			geneisClickEnabled: false,
 			bioisClickEnabled: false,
 			mondoSelected: '',
-			
 		}
+
 		this.handleMondoSearch = this.handleMondoSearch.bind(this);
 		this.handleTextChange = this.handleTextChange.bind(this);
 		this.handleMondoClick = this.handleMondoClick.bind(this);
-        this.handleGeneClick = this.handleGeneClick.bind(this);
-        
+    this.handleGeneClick = this.handleGeneClick.bind(this);
+		this.handlePathwayClick = this.handlePathwayClick.bind(this);
 	}
+
 	handleMondoSearch(e) {
 		fetch('http://127.0.0.1:5000/api/disease/'.concat(this.state.searchText).concat('?size=5'))
 			.then(response => response.json())
 			.then(data => this.setState({ mondoList: data, mondoisClickEnabled: true }));
 	}
+
 	handleTextChange(e) {
 		this.setState({searchText : e.target.value});
 	}
-	
+
 	handleMondoClick(mondoItem) {
 		fetch('http://127.0.0.1:5000/api/disease-to-gene/'.concat(mondoItem).concat('?size=5'))
 			.then(response => response.json())
 			.then(data => this.setState({ geneList: data, geneisClickEnabled: true }));
 	}
-    handleGeneClick(geneItem) {
-        fetch('http://127.0.0.1:5000/api/gene-to-pathway/'.concat(geneItem).concat('?size=5'))
-			.then(response => response.json())
-			.then(data => this.setState({ biomodelList: data, bioisClickEnabled: true }));
-    }
+
+  handleGeneClick(geneItem) {
+      fetch('http://127.0.0.1:5000/api/gene-to-pathway/'.concat(geneItem).concat('?size=5'))
+		.then(response => response.json())
+		.then(data => this.setState({ biomodelList: data, bioisClickEnabled: true }));
+  }
+
+	handlePathwayClick(index) {
+		console.log(index)
+
+	}
 
 	render() {
 		return (
 			<div className="container">
-			<SearchBar handleSearch={this.handleMondoSearch} handleTextChange={this.handleTextChange}/>		
+			<SearchBar handleSearch={this.handleMondoSearch} handleTextChange={this.handleTextChange}/>
 			<div className="row">
 				<div className="col-sm-3">
-					<MondoList 
-						mondoList={this.state.mondoList} 
+					<MondoList
+						mondoList={this.state.mondoList}
 						isClickEnabled={this.state.mondoisClickEnabled}
 						onClick={this.handleMondoClick}/>
 				</div>
 				<div className="col-sm-3">
-					<GeneList 
-                        geneList={this.state.geneList} 
-                        isClickEnabled={this.state.geneisClickEnabled} 
+					<GeneList
+                        geneList={this.state.geneList}
+                        isClickEnabled={this.state.geneisClickEnabled}
                         onClick={this.handleGeneClick}/>
 				</div>
 				<div className="col-sm-3">
-					<BioModelList biomodelList={this.state.biomodelList} isClickEnabled={this.state.bioisClickEnabled} />
+					<BioModelList
+						biomodelList={this.state.biomodelList}
+						isClickEnabled={this.state.bioisClickEnabled}
+						onClick={this.handlePathwayClick}
+					/>
 				</div>
 			</div>
 		  </div>
